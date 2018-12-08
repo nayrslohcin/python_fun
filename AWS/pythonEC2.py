60de248a8c08e9824abf23afa357d3f0027cf356
nayr.slohcin <nayr.slohcin@gmail.com>
2018-12-08 11:31:57 -0600

    Wait for instance to stop and start during snapshot process

--- AWS/pythonEC2.py
+++ AWS/pythonEC2.py
@@ -24,7 +24,52 @@ def cli():
 def volumes():
     """Commands for volumes"""
 
+@volumes.command('list')
+@click.option('--project', default=None,
+  help="Only instances for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
 
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+ 
+    return
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+  help="Only instances for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+ 
+    return
 
 @cli.group('instances')
 def instances():
@@ -32,7 +77,7 @@ def instances():
 
 @instances.command('list')
 @click.option('--project', default=None,
-    help="Only instances for project (tag Project:<name>)")
+  help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
   
@@ -79,6 +124,34 @@ def start_instances(project):
         i.start()
 
     return
+
+@instances.command('createsnap',
+  help='Create EC2 snapshots')
+@click.option('--project', default=None,
+  help='Only instances for project')
+def create_snapshots(project):
+    "Create snapshot for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("  Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by automated process")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    return
+
+    print("Job's done!")
 
 if __name__ == '__main__':
 #  print(sys.argv)
